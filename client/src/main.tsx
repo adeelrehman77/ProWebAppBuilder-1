d119102a8af07cca52db57467628f7daa8eea5d6
adeelrehman77 <38228695-adeelrehman77@users.noreply.replit.com>
2024-12-12 02:03:58 +0000

    Agent query: Can you now navigate to different pages through the sidebar without any errors?
    Add sidebar component to main application layout.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/9b26ec51-6e62-4c6b-a922-b94b547e3b73/02e6b231-1e23-4c35-811e-af29d9058908.jpg

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
+import { Sidebar } from "@/components/Sidebar";
 import AuthPage from "./pages/AuthPage";
 import DashboardPage from "./pages/DashboardPage";
 import ProductsPage from "./pages/ProductsPage";
